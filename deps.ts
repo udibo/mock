1d2c5e9c0903075c52197064dd7c9e8064ae4351
Kyle June <github@kylejune.com>
2022-03-12 21:17:12 -0600

    Update dependencies and replace Vector with Array

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { delay } from "https://deno.land/std@0.125.0/async/delay.ts";
-export type { DelayOptions } from "https://deno.land/std@0.125.0/async/delay.ts";
+export { delay } from "https://deno.land/std@0.129.0/async/delay.ts";
+export type { DelayOptions } from "https://deno.land/std@0.129.0/async/delay.ts";
 
 export {
   assert,
@@ -10,11 +10,10 @@ export {
   assertRejects,
   assertStrictEquals,
   assertThrows,
-} from "https://deno.land/std@0.125.0/testing/asserts.ts";
+} from "https://deno.land/std@0.129.0/testing/asserts.ts";
 
-export { RBTree } from "https://deno.land/x/collections@0.11.2/trees/rb_tree.ts";
-export { ascend } from "https://deno.land/x/collections@0.11.2/comparators.ts";
-export { Vector } from "https://deno.land/x/collections@0.11.2/vector.ts";
+export { RBTree } from "https://deno.land/x/collections@0.12.0/trees/rb_tree.ts";
+export { ascend } from "https://deno.land/x/collections@0.12.0/comparators.ts";
 
 export {
   applyInstanceMixins,
